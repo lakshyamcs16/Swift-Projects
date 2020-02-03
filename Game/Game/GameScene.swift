501d33d4b6a355a830508871afe7e568dd508df5
Aashna Narula <sethi.laskhya94@gmail.com>
2020-02-04 01:00:58 +0530

    Shapify with timms

--- Game/Game/GameScene.swift
+++ Game/Game/GameScene.swift
@@ -1,116 +1,283 @@
 //
 //  GameScene.swift
-//  Game
+//  Shapify
 //
-//  Created by Aashna & Lakshya on 03/02/20.
-//  Copyright © 2020 Aashna & Lakshya. All rights reserved.
+//  Created by Aashna Narula on 20.03.18.
+//  Copyright © 2018 aashnanarula. All rights reserved.
 //
 
-import GameplayKit
 import SpriteKit
-import Foundation
-import GameKit
-
-
+import UIKit
 
 public class GameScene: SKScene {
     
-    private var label : SKLabelNode!
-    var backgroundMusic: SKAudioNode!
+    var level: Int = 1
+    var timer:Int = 0
+    var logic: GameActions?
     
-    let allFig: [String] = ["1_1.png","1_3.png","1_2.png","4","8","12","16","20","24","28","32","36","40","44","48","52","56","60","64","68","72","76","80","84","88","92","96","100","104","108","112","116","120","124","128","132","136","140"]
-    let buttonNodeName = "button"
-    var lineWiseX : CGFloat = 0
-    var lineWiseY : CGFloat = 0
-    var jumpedAhead = false
+    var levelLabelNode: SKLabelNode?
+    var timerLabelNode: SKLabelNode?
+    var rightFigureNode: SKSpriteNode?
+    var deckNodes: [SKSpriteNode] = []
+    var deckNodesName: [String] = []
+    var lifeNodes: [SKSpriteNode] = []
+    var lives: Int = 3
     
     
     public override func didMove(to view: SKView) {
-        let background = SKSpriteNode(imageNamed: "img.png")
-        background.name = "background"
-        background.setScale(2.8)
-        addChild(background)
+        let action = SKAction.playSoundFileNamed("nextLevel.mp3", waitForCompletion: false)
+        self.run(action)
+        // connect nodes with scene
+        self.levelLabelNode = childNode(withName: "level") as? SKLabelNode
+        self.timerLabelNode = childNode(withName: "timer") as? SKLabelNode
+        self.rightFigureNode = childNode(withName: "rightFigure") as? SKSpriteNode
         
-        
-        let gameName = "Let's Shapify!"
-        let welcome = SKLabelNode(fontNamed: "Helvetica Neue")
-        welcome.text = gameName
-        welcome.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: welcome.frame.width * 1.25 , height: welcome.frame.height * 2.5))
-        welcome.physicsBody?.isDynamic = false
-        welcome.fontSize = 30
-        welcome.fontColor = SKColor.black
-        welcome.position = CGPoint(x: self.frame.midX, y: 300)
-        welcome.alpha = 1
-        welcome.zPosition = 3
-        addChild(welcome)
-        
-        
-        let buttonNodeName = "button"
-        let button = SKSpriteNode()
-        button.name = buttonNodeName
-        button.position = CGPoint(x: self.frame.midX, y: 200)
-        button.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: button.frame.width * 1.25 , height: button.frame.height * 2.5))
-        button.physicsBody?.isDynamic = false
-        button.alpha = 0
-        let fadeInOut = SKAction.sequence([.fadeIn(withDuration: 0.5),
-                                           .fadeOut(withDuration: 0.5)])
-        button.run(.repeatForever(fadeInOut))
-        addChild(button)
-        addLinewiseShape()
-    }
-    
-    func addLinewiseShape()
-    {
-        print(self.frame.height, self.frame.width)
-        physicsWorld.gravity = CGVector.init(dx: 0, dy: 0)
-
-        let wait = SKAction.wait(forDuration:0.02)
-        let action = SKAction.run {
-            let point = CGPoint(x: self.lineWiseX, y: self.lineWiseY)
-            self.createShape(at: point)
-            self.lineWiseX = self.lineWiseX + 30
-            if(self.lineWiseX >= self.frame.width + 50)
-            {
-                self.lineWiseX = 0
-                self.jumpedAhead = false
-                self.lineWiseY = self.lineWiseY + 30
+        enumerateChildNodes(withName: "//*") {
+            node, stop in
+            if node.name == "figure" {
+                self.deckNodes.append(node as! SKSpriteNode)
+                self.deckNodesName.append("")
             }
-            if (self.lineWiseY >= self.frame.height)
-            {
-                self.removeAllActions()
-                self.enumerateChildNodes(withName: self.buttonNodeName) { (node, stop) in
-                    let fadeInAction = SKAction.fadeIn(withDuration: 0.25)
-                    fadeInAction.timingMode = .easeInEaseOut
-                    node.run(fadeInAction, completion: {
-                        node.alpha = 1
-                    })
-                }
+            if node.name == "life" {
+                self.lifeNodes.append(node as! SKSpriteNode)
             }
         }
-        run(SKAction.repeatForever(SKAction.sequence([wait, action])))
+        // configure the label
+        self.levelLabelNode?.text = "Level : \(level)"
+        self.timerLabelNode?.text = String(timer)
+        // configure logic
+        self.logic = GameLogic()
+        logic?.setupLogic(delegate: self, deckSize: deckNodes.count)
+        // Draw sprites
+        drawDeck()
+        drawRightFigure()
+        drawLives()
+        setupTimer()
     }
     
-    func createShape(at point: CGPoint) {
+}
+
+
+// MARK: - Drawings
+extension GameScene {
+    
+    func drawDeck() {
+        for (index, node) in deckNodes.enumerated() {
+            let name = logic?.deck[index]
+            self.deckNodesName[index] = name!
+            node.texture = SKTexture(imageNamed: name!)
+            node.xScale = 1.5
+            node.yScale = 1.5
+        }
+    }
+    
+    func drawRightFigure() {
+        let name = logic?.rightFigureName
+        self.rightFigureNode?.texture = SKTexture(imageNamed: name!)
+    }
+    
+    func drawLives() {
+        if lives < 3 {
+            for index in lives...2 {
+                let node = lifeNodes[index]
+                node.alpha = 0.2
+            }
+        }
+    }
+    
+    //set timer for each level
+    func setupTimer() {
+        var timer = 0
+        if (level >= 1 && level <=  15) {
+            timer = 7
+        } else if (level >= 16 && level <=  35) {
+            timer = 10
+        } else if (level >= 36 && level <=  45) {
+            timer = 12
+        } else if (level >= 46 && level <=  65) {
+            timer = 15
+        } else if (level >= 66 && level <=  85) {
+            timer = 17
+        } else if (level >= 86 && level <=  105) {
+            timer = 20
+        } else if (level >= 106 && level <=  120) {
+            timer = 25
+        }
         
-        let action = SKAction.playSoundFileNamed("begin.mp3", waitForCompletion: false)
-        self.run(action)
+        var flag = 0
+        let runTimer = timer
+        let waitTimer = SKAction.wait(forDuration: 1)
+        let actionTimer = SKAction.run {
+            self.timerLabelNode?.text = "Time Left : \(timer)"
+            if timer == 1 {
+                flag = 1
+                let action = SKAction.playSoundFileNamed("lose.mp3", waitForCompletion: false)
+                self.run(action)
         
-        let randomIndex = GKRandomSource.sharedRandom().nextInt(upperBound: allFig.count)
-        let shape = SKSpriteNode()
-        shape.texture = SKTexture(imageNamed: allFig[randomIndex])
-        shape.name = "textureFig"
-        shape.size = CGSize(width:70, height: 70)
-        shape.position = CGPoint(x: point.x, y: point.y)
-        let maxRadius = max(shape.frame.size.width/2, shape.frame.size.height/2)
-        let interPersonSeparationConstant: CGFloat = 1.25
-        shape.physicsBody = SKPhysicsBody(circleOfRadius: maxRadius*interPersonSeparationConstant)
-        addChild(shape)
+                self.enumerateChildNodes(withName: "//*") {
+                    node, stop in
+                    if node.name == "figure" {
+                        node.run(SKAction.fadeOut(withDuration: 0.01))
+                    }
+                }
+                let timeUp = SKLabelNode(fontNamed: "HelveticaNeue-Bold")
+                timeUp.text = "Time Up..Try Again 😭"
+                timeUp.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: timeUp.frame.width * 1.25 , height: timeUp.frame.height * 2.5))
+                timeUp.physicsBody?.isDynamic = false
+                timeUp.fontSize = 24
+                timeUp.fontColor = SKColor.black
+                timeUp.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+                self.addChild(timeUp)
+                timeUp.alpha = 1
+                timeUp.zPosition = 4
+                var fadeOutAction = SKAction.fadeIn(withDuration: 1)
+                fadeOutAction.timingMode = .easeInEaseOut
+                timeUp.run(fadeOutAction, completion: {
+                    timeUp.alpha = 1
+                })
+                
+                
+                let button = ResetButton()
+                button.position = CGPoint(x: self.frame.midX, y: self.frame.midY - (timeUp.frame.height + 10))
+                button.delegate = self
+                button.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: button.frame.width * 1.25 , height: button.frame.height * 2.5))
+                button.physicsBody?.isDynamic = false
+                self.addChild(button)
+                button.alpha = 1
+                fadeOutAction = SKAction.fadeIn(withDuration: 1.5)
+                fadeOutAction.timingMode = .easeInEaseOut
+                button.run(fadeOutAction, completion: {
+                    button.alpha = 1
+                })
+                
+                //print("Game Over!!!")
+            }
+            else {
+                if flag == 1 {
+                    timer = 0
+                }
+                timer = timer - 1
+            }
+        }
+        run(SKAction.repeat(SKAction.sequence([waitTimer, actionTimer]) , count: runTimer ))
     }
 }
+
+// MARK: - Event Delegation
+extension GameScene: GameEvents {
+    
+    func userDidRightChoice(index: Int) {
+        _ = deckNodes[index]
+        let action = SKAction.playSoundFileNamed("rightFig.mp3", waitForCompletion: false)
+        self.run(action)
+        let name = deckNodes[index]
+        let textureName = deckNodesName[index]
+        name.texture = SKTexture(imageNamed: "\(textureName)S.png")
+        name.xScale = 1.0
+        name.yScale = 1.0
+    }
+    
+    func userDidWrongChoice() {
+        var action = SKAction.playSoundFileNamed("wrongFig.mp3", waitForCompletion: false)
+        self.run(action)
+        let index = lives - 1
+        let lifeNode = lifeNodes[index]
+        action = SKAction.fadeAlpha(to: 0.2, duration: 0.1)
+        lifeNode.run(action)
+        drawLives()
+    }
+    
+}
+
 
 extension GameScene {
-    func PlayButton() {
-        print("let's being the game!")
-
+    
+    func gameOver() {
+        let action = SKAction.playSoundFileNamed("lose.mp3", waitForCompletion: true)
+        self.run(action)
+        
+        let overMsg = SKLabelNode(fontNamed: "HelveticaNeue-Bold")
+        overMsg.text = "You Lose 😭"
+        overMsg.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: overMsg.frame.width * 1.25 , height: overMsg.frame.height * 2.5))
+        overMsg.physicsBody?.isDynamic = false
+        overMsg.fontSize = 50
+        overMsg.fontColor = SKColor.black
+        overMsg.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(overMsg)
+        overMsg.alpha = 1
+        overMsg.zPosition = 4
+        self.enumerateChildNodes(withName: "//*") {
+            node, stop in
+            if node.name == "figure" {
+                node.run(SKAction.fadeOut(withDuration: 0.01))
+            }
+        }
+        var fadeOutAction = SKAction.fadeIn(withDuration: 1) //SKAction.fadeOut(withDuration: 1.25)
+        fadeOutAction.timingMode = .easeInEaseOut
+        overMsg.run(fadeOutAction, completion: {
+            overMsg.alpha = 1
+        self.removeAllActions()
+        })
+        
+        
+        let button = ResetButton()
+        button.position = CGPoint(x: frame.midX, y: frame.midY - (overMsg.frame.height + 10))
+        button.delegate = self
+        button.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: button.frame.width * 1.25 , height: button.frame.height * 2.5))
+        button.physicsBody?.isDynamic = false
+        addChild(button)
+        button.alpha = 1
+        fadeOutAction = SKAction.fadeIn(withDuration: 1.5)
+        fadeOutAction.timingMode = .easeInEaseOut
+        button.run(fadeOutAction, completion: {
+            button.alpha = 1
+        })
+    }
+    
+    
+    func moveToNextLevel() {
+        let action =  SKAction.run {
+            let action1 = SKAction.playSoundFileNamed("nextLevel.mp3", waitForCompletion: false)
+            self.run(action1)
+            let transition = SKTransition.crossFade(withDuration: 0)
+            let nextLevelScene = GameScene(fileNamed:"GameScene")
+            nextLevelScene!.level = self.level + 1
+            nextLevelScene!.lives = self.lives
+            nextLevelScene!.scaleMode = SKSceneScaleMode.aspectFill
+            self.scene!.view?.presentScene(nextLevelScene!, transition: transition)
+        }
+        self.run(SKAction.sequence([SKAction.wait(forDuration : 0.35), action ]))
+        
+       
     }
 }
+
+// MARK: - Touches
+extension GameScene {
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let position = touch.location(in: self)
+            let node = self.atPoint(position)
+            if node.name == "figure" {
+                let figure = node as? SKSpriteNode
+                let index = deckNodes.firstIndex(of: figure!)
+                self.logic?.userChoose(index: index!)
+            }
+        }
+    }
+}
+
+extension GameScene: ResetButtonDelegate {
+    
+    func didTapReset(sender: ResetButton) {
+        let action = SKAction.playSoundFileNamed("popSound.mp3", waitForCompletion: false)
+        self.run(action)
+        let transition = SKTransition.crossFade(withDuration: 0)
+        let scene1 = GameScene(fileNamed:"GameScene")
+        scene1!.level = level
+        scene1!.scaleMode = SKSceneScaleMode.aspectFill
+        self.scene!.view?.presentScene(scene1!, transition: transition)
+    }
+}
+
